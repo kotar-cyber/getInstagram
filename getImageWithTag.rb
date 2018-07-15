f939f38145c504a5516f0112dd83d9e01cfcbe1f
Takuji Harayama <takuji@TakujinoMacBook-Pro.local>
2018-07-15 21:10:00 +0900

    InstagramのWebページの仕様変更に対応。ただし、ページ遷移ができず、最初の1ページ分のみ取得する仕様に変更。

--- getImageWithTag.rb
+++ getImageWithTag.rb
@@ -13,8 +13,8 @@ SEARCHTAG = '岸和田'
 POSTURL = 'https://www.instagram.com/query/'
 # 取得対象ページ
 url = 'https://www.instagram.com/explore/tags/' + URI.encode_www_form_component(SEARCHTAG)
-# 取得ページ数（1ページに12データ）
-GETPAGES = 10
+# 取得ページ数（暫定的に1ページのみの対応）
+GETPAGES = 1
 
 # 保存ファイル名（拡張子除く）
 csvfilename = Time.now.strftime("%Y%m%d%H%M%S")
@@ -47,24 +47,19 @@ end
 
 # CSVファイルのヘッダを記入
 def headerWrite(csvfilename)
- CSV.open("getInstagramData_#{csvfilename}.csv", "ab+") do |csv|
-   writeData = Array.new
-   writeData.push("ユーザID")
-   writeData.push("ユーザ名")
-   writeData.push("投稿日時（日本時間）")
-   writeData.push("ページURL")
-   writeData.push("いいねの数")
-   writeData.push("コメント数")
-   writeData.push("緯度（あれば）")
-   writeData.push("経度（あれば）")
-   writeData.push("場所のID（あれば）")
-   writeData.push("ハッシュタグ")
-   writeData.push("投稿者コメント")
-   writeData.push("リアクションコメント")
-
-   csv << writeData
+  CSV.open("getInstagramData_#{csvfilename}.csv", "ab+") do |csv|
+    writeData = Array.new
+    writeData.push("ユーザID")
+    writeData.push("投稿日時（日本時間）")
+    writeData.push("ページURL")
+    writeData.push("いいねの数")
+    writeData.push("コメント数")
+    writeData.push("ハッシュタグ")
+    writeData.push("投稿者コメント")
+ 
+    csv << writeData
+  end
  end
-end
 
 # ユーザのコメントを表示
 # 複数件ある場合は改行で区切る
@@ -107,52 +102,28 @@ def csvWrite(dataArray, csvfilename)
 	dataArray.length.times {|i|
 	  CSV.open("getInstagramData_#{csvfilename}.csv", "ab+") do |csv|
 	    writeData = Array.new
-	    # ユーザ情報の取得（フルネームは無い場合がある）
-	    writeData.push(dataArray[i]["owner"]["username"])
-	    if dataArray[i]["owner"]["full_name"]
-	      writeData.push("\""+dataArray[i]["owner"]["full_name"]+"\"")
-	    else
-	      writeData.push("")
-	    end
+	    # ユーザIDの取得
+	    writeData.push(dataArray[i]['node']['owner']['id'])
 
 	    # UNIXタイムからの変換
-	    writeData.push(Time.at(dataArray[i]["date"]))
+	    writeData.push(Time.at(dataArray[i]['node']['taken_at_timestamp']))
 
 	    # 画像URLの取得
 	    # writeData.push(dataArray[i]["thumbnail"])
 
       # ページURLの取得
-      writeData.push("https://www.instagram.com/p/" + dataArray[i]["code"] + "/")
+      writeData.push("https://www.instagram.com/p/" + dataArray[i]['node']['shortcode'] + "/")
 
 
 	    # いいねの数とコメントの数
-	    writeData.push(dataArray[i]["likes"]["count"])
-	    writeData.push(dataArray[i]["comments"]["count"])
-
-
-	    # 位置情報の取得（位置情報は無い場合がある）
-	    if dataArray[i]["location"]
-	      writeData.push(dataArray[i]["location"]["lat"]) if dataArray[i]["location"]["lat"]
-	      writeData.push(dataArray[i]["location"]["lng"]) if dataArray[i]["location"]["lng"]
-	      writeData.push(dataArray[i]["location"]["id"]) if dataArray[i]["location"]["id"]
-	    else
-	      3.times {
-	        writeData.push("")
-	      }
-	    end
-
-
+      writeData.push(dataArray[i]['node']['edge_liked_by']['count'])
+      writeData.push(dataArray[i]['node']['edge_media_to_comment']['count'])
+	    
       # 投稿者コメントからタグのみ抽出
-	    writeData.push((dataArray[i]["caption"] + " ").scan(/[#][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー○]+/).join(" "))
+	    writeData.push((dataArray[i]['node']['edge_media_to_caption']['edges'][0]['node']['text'] + " ").scan(/[#][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー○]+/).join(" "))
 	    # 投稿者コメントの取得
-	    writeData.push("\""+dataArray[i]["caption"]+"\"")
+	    writeData.push("\""+dataArray[i]['node']['edge_media_to_caption']['edges'][0]['node']['text']+"\"")
 
-	    # リアクションコメントがある場合は取得する
-	    if dataArray[i]["comments"]["count"] != 0
-	      pageUrl = 'https://www.instagram.com/p/' + dataArray[i]["code"]
-	      userComments = getUserComments(pageUrl)
-	      writeData.push(userComments)
-	    end
 
 	    csv << writeData
 	  end
@@ -197,33 +168,17 @@ end
 # 全部のHTMLを取得
 allDoc = Nokogiri::HTML.parse(html, nil, charset)
 # メタ情報だけ取得
-metaInfo = allDoc.css('script')[6].text
+metaInfo = allDoc.css('body script').first.text
 # 前後に不要な情報があるのでカット
 metaInfo.slice!(0, 21)
 metaInfo = metaInfo.chop
-# csrfトークンの取得
-csrf_token = JSON.parse(metaInfo)['config']['csrf_token']
 
-#end_cursor = JSON.parse(metaInfo)['entry_data']['TagPage'][0]['tag']['media']['page_info']['end_cursor']
-start_cursor = JSON.parse(metaInfo)['entry_data']['TagPage'][0]['tag']['media']['page_info']['start_cursor']
-getDataLength = JSON.parse(metaInfo)['entry_data']['TagPage'][0]['tag']['media']['nodes'].length
-
-dataArray = parseInstagramJson(JSON.parse(metaInfo)['entry_data']['TagPage'][0]['tag']['media']['nodes'])
+dataArray = JSON.parse(metaInfo)['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']['edges'];
 
 headerWrite(csvfilename)
 
-# 取得回数分取得
-GETPAGES.times { |i|
- puts "データ取得 #{i+1} / #{GETPAGES}"
- returnData = getNextPage(start_cursor, csrf_token)
+puts "データ取得します"
 
- dataArray = parseInstagramJson(returnData[0])
- csvWrite(dataArray, csvfilename)
-
- start_cursor = returnData[1]
-
- sleep 5
-
-}
+csvWrite(dataArray, csvfilename)
 
 puts "終了しました"
